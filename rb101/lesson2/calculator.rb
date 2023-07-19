b8a1ae2a8514ede43f9306ddd94c42861868a02b
Ang Sherpa <anglamusherpa@gmail.com>
2023-07-19 15:58:41 -0600

    Edited calculator assignment after code review

--- rb101/lesson2/calculator.rb
+++ rb101/lesson2/calculator.rb
@@ -3,14 +3,20 @@
 require 'yaml'
 
 MESSAGES = YAML.load_file('calculator_msg.yml')
+CHOICES = %w[1 2 3 4].freeze
 
 def prompt(message, personalize = '')
   puts "=> #{MESSAGES[message] + personalize}"
 end
 
+def welcome
+  prompt('welcome')
+  prompt('get_name')
+end
+
 def get_valid_name(name)
   loop do
-    return name unless name.empty?
+    return name unless name.strip.empty?
 
     prompt('invalid_name')
     name = gets.chomp
@@ -18,7 +24,7 @@ def get_valid_name(name)
 end
 
 def valid_number?(input)
-  input.to_f.to_s == input || input.to_i.to_s == input
+  /\d/.match(input) && /^-?\d*\.?\d*$/.match(input)
 end
 
 def get_valid_number(number)
@@ -30,23 +36,39 @@ def get_valid_number(number)
   end
 end
 
+def set_first_number
+  prompt('first_num')
+  first_number = gets.chomp
+  get_valid_number(first_number)
+end
+
+def set_second_number
+  prompt('second_num')
+  second_number = gets.chomp
+  get_valid_number(second_number)
+end
+
 def get_valid_operator(operator)
   loop do
-    return operator if %w[1 2 3 4].include?(operator)
+    return operator if CHOICES.include?(operator)
 
     prompt('invalid_op')
     operator = gets.chomp
   end
 end
 
-def operation_to_message(operator, first_number, second_number)
-  operation = case operator
-              when '1' then 'Adding'
-              when '2' then 'Subtracting'
-              when '3' then 'Multiplying'
-              when '4' then 'Dividing'
-              end
+def set_operator
+  prompt('operator_prompt')
+  operator = gets.chomp
+  get_valid_operator(operator)
+end
 
+def operation_to_message(operator, first_number, second_number)
+  operation_msg = { '1' => 'Adding',
+                    '2' => 'Subtracting',
+                    '3' => 'Multiplying',
+                    '4' => 'Dividing' }
+  operation = operation_msg[operator] 
   puts "=> #{operation} #{first_number} and #{second_number} ..."
 end
 
@@ -60,53 +82,53 @@ def calculate_result(operator, first_number, second_number)
 end
 
 def zero_division?(operator, second_number)
-  operator == '4' && second_number.to_i.zero?
+  operator == '4' && second_number.to_f.zero?
 end
 
 def get_nonzero_number(operator, second_number)
   loop do
-    return second_number unless operator == '4' && second_number.to_i.zero?
+    return second_number unless zero_division?(operator, second_number)
 
     prompt('zero_denom')
     second_number = gets.chomp
   end
 end
 
-def display_result(result)
-  puts "=> The final result is #{result.round(4)}"
+def display_result(operator, first_number, second_number)
+  result = calculate_result(operator, first_number, second_number)
+  puts "=> The final result is #{result.round(5)}"
 end
 
 # Start of main program
-prompt('welcome')
-prompt('get_name')
+welcome()
+
 name = gets.chomp
-name = get_valid_name(name)
+name = get_valid_name(name).capitalize
 
 prompt('user_name', "#{name}!")
 
 # main loop
 loop do
-  prompt('first_num')
-  first_number = gets.chomp
-  first_number = get_valid_number(first_number).to_f
+  first_number = set_first_number().to_f
 
-  prompt('second_num')
-  second_number = gets.chomp
-  second_number = get_valid_number(second_number).to_f
+  second_number = set_second_number().to_f
 
-  prompt('operator_prompt')
-  operator = gets.chomp
-  operator = get_valid_operator(operator)
+  operator = set_operator()
 
   second_number = get_nonzero_number(operator, second_number).to_f if zero_division?(operator, second_number)
 
+  system('clear')
+
   operation_to_message(operator, first_number, second_number)
-  result = calculate_result(operator, first_number, second_number)
-  display_result(result)
+  display_result(operator, first_number, second_number)
 
   prompt('continue?')
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
+
+  system('clear')
 end
 
 prompt('goodbye', "#{name}!")
+
+# End
