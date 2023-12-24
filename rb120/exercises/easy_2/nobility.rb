3277b0a5ad5a0cfdee8e103f40b66ef84342cc75
Ang Sherpa <anglamusherpa@gmail.com>
2023-12-24 12:08:53 -0700

    All exercises from RB120 Easy 2

---
+++ rb120/exercises/easy_2/nobility.rb
@@ -0,0 +1,84 @@
+module Walkable
+  def walk
+    "#{name} #{gait} forwards"
+  end
+end
+
+class Person
+  include Walkable
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  private
+
+  def gait
+    "strolls"
+  end
+end
+
+class Noble < Person
+  def initialize(name, title)
+    super(name)
+    @title = title
+  end
+
+  def name
+    "#{@title} #{@name}"
+  end
+
+  def gait
+    "struts"
+  end
+end
+
+class Cat
+  include Walkable
+  
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  private
+
+  def gait
+    "saunters"
+  end
+end
+
+class Cheetah
+  include Walkable
+  
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  private
+
+  def gait
+    "runs"
+  end
+end
+
+mike = Person.new("Mike")
+mike.walk
+# => "Mike strolls forward"
+
+kitty = Cat.new("Kitty")
+kitty.walk
+# => "Kitty saunters forward"
+
+flash = Cheetah.new("Flash")
+flash.walk
+# => "Flash runs forward"
+
+byron = Noble.new("Byron", "Lord")
+puts byron.walk
+# => "Lord Byron struts forward"
---
+++ rb120/exercises/easy_2/what_will_this_do.rb
@@ -0,0 +1,17 @@
+class Something
+  def initialize
+    @data = 'Hello'
+  end
+
+  def dupdata           # instance method
+    @data + @data
+  end
+
+  def self.dupdata      # class method
+    'ByeBye'
+  end
+end
+
+thing = Something.new
+puts Something.dupdata # ByeBye
+puts thing.dupdata # HelloHello
